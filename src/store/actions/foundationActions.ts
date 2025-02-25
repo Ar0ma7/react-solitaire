576674a2e860e19c771c0eaf1956106fb1198a1e
Aroma <aroma.xxx.777@gmail.com>
2025-02-26 02:16:12 +0900

    win check

--- src/store/actions/foundationActions.ts
+++ src/store/actions/foundationActions.ts
@@ -1,8 +1,17 @@
 import { state } from '../store';
 
+const checkWin = () => {
+	const isWin = state.foundations.every((foundation) => foundation.length === 13);
+	if (isWin) {
+		alert('You Win!');
+	}
+};
+
 const moveToFoundation = (areaIndex: number) => {
 	state.foundations[areaIndex] = [...state.foundations[areaIndex], state.movingCardList[0]];
 	state.movingCardList = [];
+
+	checkWin();
 };
 
 export const foundationActions = {
